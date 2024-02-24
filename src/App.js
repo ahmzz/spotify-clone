74278963259722072021379fce69277d1fb0f103
Ahmed Imran <ahmedimran.centrox@gmail.com>
2024-02-25 00:23:01 +0500

    Context API implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import { getTokenFromUrl } from "./Spotify/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./pages/Player/Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -9,9 +13,15 @@ function App() {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const hashToken = hash.access_token;
-    if (hashToken) setToken(hashToken);
+    if (hashToken) {
+      setToken(hashToken);
+      spotify.setAccessToken(hashToken)
+
+      spotify.getMe()
+      .then(user=>console.log(user))
+    }
   }, []);
-  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
+  return <div className="app">{token ? <Player/> : <Login />}</div>;
 }
 
 export default App;
